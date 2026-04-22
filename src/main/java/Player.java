a84d5acc0bdb843b80339e8ed9769d6127fd183f
Fletcher Cahill <9fletch88@gmail.com>
2026-04-21 20:12:31 -0700

    Implemented functioning linear movement.

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,32 +1,59 @@
 public class Player {
-    private int health;
-    private int positionX;
-    private int positionY;
-    private boolean isJumping;
-    private boolean blastUsed;
+    private int health = 100;
+    private int positionX = 100;
+    private int positionY = 500; // ground level
+    private boolean isJumping = false;
+    private boolean blastUsed = false;
     private String name;
 
-    public void moveLeft()
-    {
-        //
-    }
-    public void moveRight(){
-        //
-    }
-    public void jump(){
-        //
-    }
-    public void kick(){
-        //
-    }
-    public void punch(){
-        //
-    }
-    public void useBlast(){
-        //
-    }
-    public void takeDamage(){
-        //
+    private static final int MOVE_SPEED = 10;
+    private static final int JUMP_HEIGHT = 150;
+
+    public void moveLeft() {
+        positionX -= MOVE_SPEED;
     }
 
+    public void moveRight() {
+        positionX += MOVE_SPEED;
+    }
+
+    public void jump() {
+        if (!isJumping) {
+            isJumping = true;
+            positionY -= JUMP_HEIGHT;
+
+            // simple "gravity" reset (you'll improve this later)
+            positionY += JUMP_HEIGHT;
+            isJumping = false;
+        }
+    }
+
+    public void kick() {
+        System.out.println(name + " kicks!");
+        // later: check collision with opponent
+    }
+
+    public void punch() {
+        System.out.println(name + " punches!");
+        // later: check collision with opponent
+    }
+
+    public void useBlast() {
+        if (!blastUsed) {
+            System.out.println(name + " uses special blast!");
+            blastUsed = true;
+        } else {
+            System.out.println("Blast already used!");
+        }
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+        if (health < 0) health = 0;
+    }
+
+    // Getters (important for drawing later)
+    public int getX() { return positionX; }
+    public int getY() { return positionY; }
+    public int getHealth() { return health; }
 }
